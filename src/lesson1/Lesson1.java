f521296c2624f11d6ee9b774baa27b6b6949c6bc
DungCR7 <buivandung279@gmail.com>
2021-11-26 15:33:53 +0700

    Init project

---
+++ src/lesson1/Lesson1.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+
+        int a = 20;
+        byte c = (byte) a;
+        System.out.println(c);
+    }
+}
